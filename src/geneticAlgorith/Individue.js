faaf8a692220cfb518be8a958e78ed07e6fe64c5
Daedra <foglzerika@gmail.com>
2020-03-13 12:24:21 -0600

    Creating repo

---
+++ src/geneticAlgorith/Individue.js
@@ -0,0 +1,14 @@
+import {pl} from './pl'
+console.log("hola")
+console.log(pl)
+
+export class Indiviude{
+    constructor(){
+        let cromosome=[]
+    }
+    create = () =>{
+        for(let i =0 ; i <= 3 ; i++){
+
+        }
+    }
+}
---
+++ src/geneticAlgorith/pl.js
@@ -0,0 +1,27 @@
+const pl =[
+    {
+        id:1,
+        name: "Torta",
+        price: 45,
+        like: 1
+    },
+    {
+        id:2,
+        name: "Tacos(3}",
+        price: 30,
+        like: 2
+    },
+    {
+        id:3,
+        name: "Pambazo",
+        price: 20,
+        like: 3
+    },
+    {
+        id:4,
+        name: "Pizza",
+        price: 60,
+        like: 1
+    },
+
+]
